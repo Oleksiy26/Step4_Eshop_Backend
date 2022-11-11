d9caa4812ae25470f0c059743ec1b0b35293531f
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-11-11 15:42:22 +0200

    add file products.json

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://Oleksii:Qazxswedc351@cluster0.pkff6fd.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://Oleksii:Qazxswedc351@cluster0.pkff6fd.mongodb.net/step4data?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
