3980f4d96e7afdad8608c09b26e3e3409bec4bdc
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-11-23 14:48:42 +0200

    Slider Schema changes

--- models/Slider.js
+++ models/Slider.js
@@ -1,37 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SliderSchema = new Schema(
   {
     customId: {
       type: String,
-      required: true
+      required: true,
     },
     title: String,
-    imageUrl: {
-      type: String,
-      required: true
-    },
+    imageUrl: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
     description: String,
     htmlContent: String,
     product: {
       type: Schema.Types.ObjectId,
-      ref: "products"
+      ref: 'products',
     },
     category: {
       type: Schema.Types.ObjectId,
-      ref: "catalogs"
+      ref: 'catalogs',
     },
     customer: {
       type: Schema.Types.ObjectId,
-      ref: "customers"
+      ref: 'customers',
     },
     date: {
       type: Date,
-      default: Date.now
-    }
+      default: Date.now,
+    },
   },
   { strict: false }
 );
 
-module.exports = Slider = mongoose.model("slides", SliderSchema, "slides");
+module.exports = Slider = mongoose.model('slides', SliderSchema, 'slides');
